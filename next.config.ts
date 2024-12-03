fad278c8c294b579d80ce239fe4fbbcde329610b
Akaikenlol <keithken972@gmail.com>
2024-12-03 21:14:25 +0630

    feat: Implementing FileUpload functionality

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
 			},
 			{
 				protocol: "https",
+				hostname: "static.vecteezy.com",
+			},
+			{
+				protocol: "https",
 				hostname: "cloud.appwrite.io",
 			},
 		],
