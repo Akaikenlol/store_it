d3c5da4c6a90520d3dddbeef7a1f0678d7be1767
Akaikenlol <keithken972@gmail.com>
2024-12-06 22:00:46 +0630

    feat: Implementing Action Dropdown

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+"use client";
 
-const ActionDropdown = () => {
-	return <div>ActionDropdown</div>;
+import React, { useState } from "react";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Image from "next/image";
+import { Models } from "node-appwrite";
+import { actionsDropdownItems } from "@/constants";
+
+const ActionDropdown = ({ file }: { file: Models.Document }) => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+	return (
+		<Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+			<DropdownMenu open={isDropdownOpen}>
+				<DropdownMenuTrigger className={"shad-no-focus"}>
+					<Image
+						src={"/assets/icons/dots.svg"}
+						alt="dots"
+						width={34}
+						height={34}
+					/>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent>
+					<DropdownMenuLabel className="max-w-[200px]">
+						{file.name}
+					</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					{actionsDropdownItems.map((actionItem) => (
+						<DropdownMenuItem
+							key={actionItem.value}
+							className="shad-dropdown-item"
+						>
+							{actionItem.label}
+						</DropdownMenuItem>
+					))}
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</Dialog>
+	);
 };
 
 export default ActionDropdown;
